ed05b49f764df34ee3eb8dad4858d078e7c1aef7
Sergio De La Hoz <sdelahoz@nimocom.com>
2019-06-17 16:34:10 -0500

    Query de accounts en GraphQL

---
+++ Wallet.GraphQL/AppSchema.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using Wallet.GraphQL.Queries;
+
+namespace Wallet.GraphQL
+{
+    public class AppSchema : Schema
+    {
+        public AppSchema(IDependencyResolver resolver)
+        : base(resolver)
+        {
+            Query = resolver.Resolve<AppQuery>();
+        }
+    }
+}
---
+++ Wallet.GraphQL/Queries/AppQuery.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using Wallet.Data.Entities;
+using Wallet.GraphQL.Types;
+using Wallet.Services.Core;
+
+namespace Wallet.GraphQL.Queries
+{
+    public class AppQuery : ObjectGraphType
+    {
+        public AppQuery(IEntityService<Account> repository)
+        {
+            Field<ListGraphType<AccountGQL>>(
+               "accounts",
+               resolve: context => repository.GetAllAsync()
+           );
+        }
+    }
+}
