c3684e5e52f0e6bcfb7c80445ca43a8bd404903d
Sergio De La Hoz <sdelahoz@nimocom.com>
2019-06-17 12:46:07 -0500

    - Se agrega projecto de servicios

---
+++ Wallet.Services/Extensions/StartUpConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wallet.Data;
+
+namespace Wallet.Services.Extensions
+{
+    public static class StartUpConfiguration
+    {
+        public static void ConfigureDBConnection(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<WalletContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
